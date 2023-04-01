ac8e230fde0b1af5848c4c078bd0d164d9408a01
Denis Shumeev <denis-shumeev@yandex.ru>
2023-04-02 01:39:02 +0300

    Move secret key into .env.

--- sunny_days/sunny_days/settings.py
+++ sunny_days/sunny_days/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-khw@49*es4#mjd=kwb&33=$o5isaclky&_%ax+wior30)ue^s5'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
